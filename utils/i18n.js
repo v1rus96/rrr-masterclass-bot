f70eed1e7c6062b9f54b3128be01e3bc94361df3
nameapp <email@email>
2024-11-26 22:40:45 +0500

    localication

--- utils/i18n.js
+++ utils/i18n.js
@@ -2,7 +2,7 @@ const i18n = require("i18n");
 
 i18n.configure({
   locales: ["en", "uz", "ru"],  // Add your supported languages here
-  directory: __dirname + "../locales",         // Directory to store your language files
+  directory: __dirname + "/locales",         // Directory to store your language files
   defaultLocale: "uz",                       // Default language
   objectNotation: true,                      // Support for nested keys
 });
