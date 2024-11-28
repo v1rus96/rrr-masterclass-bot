ba6f665355b15a06990575f543d125d097f32fda
nameapp <email@email>
2024-11-28 14:20:01 +0500

    broadcast

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const User = require("./models/User"); // Import your User model
 const i18n = require("./utils/i18n"); // Import i18n for localization
 const logger = require("./utils/logger"); // Import logger
 const Message = require("./models/Message"); // Import the Message model
-const ADMIN_CHAT_ID = 140251378 || 6055606466; // Replace with your admin chat ID
+const ADMIN_CHAT_ID = 6055606466; // Replace with your admin chat ID
 const { supabase } = require("./config/db");
 // When user starts the bot
 bot.onText(/\/start/, onStart);
